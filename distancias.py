608b1d926fff51c7388878cde9045fa576cb677a
Jaime <jamiddleton@uc.cl>
2021-01-07 13:34:32 -0300

    comments

--- distancias.py
+++ distancias.py
@@ -5,13 +5,14 @@ class distancias:
     def __init__(self):
         self.network = nx.read_gpickle("network.gpickle")
     
+    # p1, p2 son un par-cordenadas (lat, lon)
     def calculate(self, p1, p2):
         orig_node = ox.get_nearest_node(self.network, p1)
         dest_node = ox.get_nearest_node(self.network, p2)
         dist = nx.shortest_path_length(self.network, orig_node, dest_node, weight = 'length')
         return dist
 
-    # p1 punto norte-oeste, p2 punto sur-este
+    # p1 punto norte-oeste, p2 punto sur-este. Son pares de coordenadas (lat, lon)
     def reload_map(self, p1, p2):
         try:
             network = ox.graph_from_bbox(p1[0], p2[0], p2[1], p1[1], custom_filter = '["highway"~"primary|secondary|tertiary"]')
