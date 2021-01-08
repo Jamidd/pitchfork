3979634c8fd704944cb6bf5c7fd354ab1085752d
Jaime <jamiddleton@uc.cl>
2021-01-08 01:21:36 -0300

    variable

--- webpage/routes/simulate.js
+++ webpage/routes/simulate.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('simulate', { title: 'Express' });
+router.post('/', function(req, res) {
+  res.render('simulate', { title: 'Express', address: req.body.address});
 });
+
 router.get('/log', function(req, res, next) {
-  res.render('log', { title: 'Express' });
+  res.render('log', { title: 'Express'});
 });
 
 module.exports = router;
